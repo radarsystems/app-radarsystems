a0cfb53247d931094a3ee4ea38e1f97e3ec93234
Baalberith <72678093+baalbvip@users.noreply.github.com>
2023-10-06 13:26:56 -0400

    sadsad

--- src/Pages/App/Home/Home.jsx
+++ src/Pages/App/Home/Home.jsx
@@ -12,6 +12,75 @@ export default function Home() {
                     <span>Bienvenido a RadarSystems aca podras ver todas tus estadisticas y estados de tu cuenta</span>
                 </div>
             </div>
+
+            <div className="body-stat">
+                <div className="row">
+                    <div className="col-md-3">
+                        <div className="box stat box-padding">
+                            <div className="top">
+                                <p>Correos Enviados</p>
+                            </div>
+
+                            <div className="resp-number">
+                                <p>1</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-3">
+                        <div className="box stat box-padding">
+                            <div className="top">
+                                <p>SMS Enviados</p>
+                            </div>
+
+                            <div className="resp-number">
+                                <p>12</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-3">
+                        <div className="box stat box-padding">
+                            <div className="top">
+                                <p>Campaigns Email</p>
+                            </div>
+
+                            <div className="resp-number">
+                                <p>13</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-3">
+                        <div className="box stat box-padding">
+                            <div className="top">
+                                <p>Campaigns Sms</p>
+                            </div>
+
+                            <div className="resp-number">
+                                <p>1</p>
+                            </div>
+                        </div>
+                    </div>
+                    
+
+                    <div className="col-md-6">
+                        <div className="box stat box-padding">
+                            <div className="top">
+                                <p>Top's Campaigns SMS</p>
+                                <span>Ve las campanas mas demandadas</span>
+                                <span></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-md-6">
+                        <div className="box stat box-padding">
+                            <div className="top">
+                                <p>Top's Campaigns SMS</p>
+                                <span>Ve las campanas mas demandadas</span>
+                                <span></span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }
