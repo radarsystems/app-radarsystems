083351c48049efdf6f1e4b1e051e1a8ad7aff3f2
Baalberith <72678093+baalbvip@users.noreply.github.com>
2024-02-23 14:09:01 -0400

    asdasasd

--- src/Components/HeaderLeft.jsx
+++ src/Components/HeaderLeft.jsx
@@ -112,7 +112,7 @@ export default function HeaderLeft({ MenuLeft }) {
                         </ul>
                     </li>
 
-                    <li className={opacityMenu ? 'opacity' : ''}>
+                    <li className={opacityMenu ? 'opacity' : 'none'}>
                         <button onClick={(ev) => { ToggleChild(ev); }}><i className="icon"><IoSendOutline /></i> Campañas <i className="arrow"><MdArrowForwardIos /></i></button>
 
                         <ul>
@@ -124,11 +124,11 @@ export default function HeaderLeft({ MenuLeft }) {
                         </ul>
                     </li>
 
-                    <li >
+                    <li className="none">
                         <button onClick={(ev) => { window.location.href = "https://workflows.radarsystems.net/" }}><i className="icon"><Icon icon="octicon:workflow-16" /> </i> Workflow <i className="arrow"><MdArrowForwardIos /></i></button>
                     </li>
 
-                    <li className={opacityMenu ? 'opacity' : ''}><button onClick={ToggleChild}><i className="icon"><IoPersonOutline /></i> Listas <i className="arrow"><MdArrowForwardIos /></i></button>
+                    <li className={opacityMenu ? 'opacity' : 'none'}><button onClick={ToggleChild}><i className="icon"><IoPersonOutline /></i> Listas <i className="arrow"><MdArrowForwardIos /></i></button>
 
                         <ul>
                             <li><Link to="/contacts">Mis Contactos</Link></li>
@@ -195,7 +195,7 @@ export default function HeaderLeft({ MenuLeft }) {
                     </li>
 
 
-                    <li className={"none"}>
+                    <li className={""}>
                         <button onClick={ToggleChild}><i className="icon"><Icon icon="mdi:jellyfish-outline" /></i> Medusa <i className="arrow"><MdArrowForwardIos /></i></button>
                         <ul>
                             <li><Link to="/shorturls/campaigns">Mis Campañas</Link></li>
